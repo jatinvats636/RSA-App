fe9e04ffebc36b745a8506460607070b9ea25fd9
jatin vats <500068636@stu.upes.ac.in>
2021-04-06 19:03:02 +0530

    updated files

--- RSA/app/src/main/java/com/example/rsa/MainActivity.java
+++ RSA/app/src/main/java/com/example/rsa/MainActivity.java
@@ -28,21 +28,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int i, n, on, e, d=0;    //// e(public key exponent) & d(private key exponent)
+                double c;
                 int p = Integer.parseInt(String.valueOf(pText.getText()));
                 int q = Integer.parseInt(String.valueOf(qText.getText()));
                 int plainText = Integer.parseInt(String.valueOf(msgText.getText()));
                 resText.setText("p : "+p +"\n"+ "q : "+q);
-
                 n = p * q;
                 on = (p - 1) * (q - 1);
                 resText.append("\nn : "+n +"\n"+ "ϕ(n) : "+on);
-
                 for (e = 2; e < on; e++) {
                     if (gcd(e, on) == 1) {
                         break;
                     }
                 }
                 resText.append("\ne : "+e);
+                for (i = 0; i <= 9; i++) {
+                    int x = 1 + (i * on);
+                    if (x % e == 0) {
+                        d = x / e;
+                        break;
+                    }
+                }
+                resText.append("\nd : "+d);
+                c = (Math.pow(plainText, e)) % n;
             }
         });
     }
